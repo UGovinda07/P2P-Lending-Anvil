ffd1ee75c56bf91a92868320f73b40dcf21c333d
govindapatro2000@gmail.com <govindapatro2000@gmail.com>
2024-06-28 11:10:41 +0000

    Created routing_setup; Deleted Module3

--- client_code/routing_setup.py
+++ client_code/routing_setup.py
@@ -1,10 +1,14 @@
 import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-import anvil.users 
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil_extras import routing
+from .main_form import MainForm
+from .login_page import LoginPage
+from .signup_page import SignupPage
 # This is a module.
 # You can define variables and functions here, and use them from any form. For example, in a top-level form:
 #
@@ -16,3 +20,10 @@ from anvil.tables import app_tables
 
 def say_hello():
   print("Hello, world")
+  
+routing.routes = {
+    '': MainForm,         # Default route, loads MainForm initially
+    'main': MainForm,     # Route for MainForm
+    'login': LoginPage,   # Route for LoginPage
+    'signup': SignupPage  # Route for SignupPage
+}
